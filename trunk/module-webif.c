Treboada/oscam-svn

commit a8d259852d05bfada0e1ca470624cf2f6ba29556

newcamd/webif: Move client id resolving function.
git-svn-id: https://svn.streamboard.tv/oscam@7495 4b0bc96b-bc66-0410-9d44-ebda105a78c1

trunk/module-webif.c
@@ -2092,6 +2092,28 @@ static char *send_oscam_user_config_edit(struct templatevars *vars, struct uripa
 
 }
 
+static void webif_add_client_proto(struct templatevars *vars, struct s_client *cl, char *proto) {
+#ifdef MODULE_NEWCAMD
+	if (streq(proto, "newcamd") && cl->typ == 'c') {
+		tpl_printf(vars, TPLADDONCE, "CLIENTPROTO","%s (%s)", proto, newcamd_get_client_name(cl->ncd_client_id));
+		return;
+	}
+#endif
+#ifdef MODULE_CCCAM
+	if (streq(proto,"cccam")) {
+		struct cc_data *cc = cl->cc;
+		if (cc && cc->remote_version && cc->remote_build) {
+			tpl_printf(vars, TPLADDONCE, "CLIENTPROTO", "%s (%s-%s)", proto, cc->remote_version, cc->remote_build);
+			tpl_addVar(vars, TPLADDONCE, "CLIENTPROTOTITLE", cc->extended_mode ? cc->remote_oscam : "");
+			return;
+		}
+	}
+#endif
+	(void)cl; // Prevent warning when NEWCAMD and CCCAM are both disabled
+	tpl_addVar(vars, TPLADDONCE, "CLIENTPROTO", proto);
+	tpl_addVar(vars, TPLADDONCE, "CLIENTPROTOTITLE", "");
+}
+
 static char *send_oscam_user_config(struct templatevars *vars, struct uriparams *params, int32_t apicall) {
 	struct s_auth *account;
 	struct s_client *cl;
@@ -2363,29 +2385,7 @@ static char *send_oscam_user_config(struct templatevars *vars, struct uriparams 
 				tpl_addVar(vars, TPLADD, "CLIENTPICON", "");
 			}
 
-			if ((strcmp(proto,"newcamd") == 0) && (latestclient->typ == 'c'))
-				tpl_printf(vars, TPLADDONCE, "CLIENTPROTO","%s (%s)", proto, get_ncd_client_name(latestclient->ncd_client_id));
-#ifdef MODULE_CCCAM
-			else if ((strncmp(proto,"cccam", 5) == 0)) {
-				struct cc_data *cc = latestclient->cc;
-				if(cc && cc->remote_version && cc->remote_build) {
-					tpl_printf(vars, TPLADDONCE, "CLIENTPROTO", "%s (%s-%s)", proto, cc->remote_version, cc->remote_build);
-					if(cc->extended_mode)
-						tpl_addVar(vars, TPLADDONCE, "CLIENTPROTOTITLE", cc->remote_oscam);
-					else
-						tpl_addVar(vars, TPLADDONCE, "CLIENTPROTOTITLE", ""); //unset tpl var
-				}
-				else
-				{
-					tpl_addVar(vars, TPLADDONCE, "CLIENTPROTO", proto);
-					tpl_addVar(vars, TPLADDONCE, "CLIENTPROTOTITLE", "");
-				}
-			}
-#endif
-			else {
-				tpl_addVar(vars, TPLADDONCE, "CLIENTPROTO", proto);
-				tpl_addVar(vars, TPLADDONCE, "CLIENTPROTOTITLE", "");
-			}
+			webif_add_client_proto(vars, latestclient, proto);
 		}
 
 		tpl_addVar(vars, TPLADD, "CLASSNAME", classname);
@@ -2986,30 +2986,7 @@ static char *send_oscam_status(struct templatevars *vars, struct uriparams *para
 				tpl_addVar(vars, TPLADD, "CLIENTIP", cs_inet_ntoa(cl->ip));
 				tpl_printf(vars, TPLADD, "CLIENTPORT", "%d", cl->port);
 				char *proto = monitor_get_proto(cl);
-
-				if ((strcmp(proto,"newcamd") == 0) && (cl->typ == 'c'))
-					tpl_printf(vars, TPLADD, "CLIENTPROTO","%s (%s)", proto, get_ncd_client_name(cl->ncd_client_id));
-#ifdef MODULE_CCCAM
-				else if ((strncmp(proto,"cccam", 5) == 0)) {
-					struct cc_data *cc = cl->cc;
-					if(cc && cc->remote_version && cc->remote_build) {
-						tpl_printf(vars, TPLADD, "CLIENTPROTO", "%s (%s-%s)", proto, cc->remote_version, cc->remote_build);
-						if(cc->extended_mode)
-							tpl_addVar(vars, TPLADD, "CLIENTPROTOTITLE", cc->remote_oscam);
-						else
-							tpl_addVar(vars, TPLADD, "CLIENTPROTOTITLE", ""); //unset tpl var
-					}
-					else
-					{
-						tpl_addVar(vars, TPLADD, "CLIENTPROTO", proto);
-						tpl_addVar(vars, TPLADD, "CLIENTPROTOTITLE", "");
-					}
-				}
-#endif
-				else {
-					tpl_addVar(vars, TPLADD, "CLIENTPROTO", proto);
-					tpl_addVar(vars, TPLADD, "CLIENTPROTOTITLE", "");
-				}
+				webif_add_client_proto(vars, cl, proto);
 
 				if (!apicall) {
 					if((cl->typ != 'p' && cl->typ != 'r') || cl->reader->card_status == CARD_INSERTED){

trunk/global-functions.h
@@ -381,7 +381,6 @@ extern void init_rnd(void);
 extern int32_t hexserialset(struct s_reader *rdr);
 extern char *monitor_get_proto(struct s_client *);
 extern char *reader_get_type_desc(struct s_reader * rdr, int32_t extended);
-extern char *get_ncd_client_name(char *client_id);
 extern int32_t cs_strnicmp(const char * str1, const char * str2, size_t num);
 extern char *strnew(char *str);
 extern void hexserial_to_newcamd(uchar *source, uchar *dest, uint16_t caid);
@@ -419,6 +418,11 @@ extern int32_t format_cxm(struct s_cacheex_matcher *entry, char *result, size_t 
 extern int8_t cs_cacheex_maxhop(struct s_client *cl);
 
 extern int streq(const char *s1, const char *s2);
+
+/* ===========================
+ *       module-newcamd
+ * =========================== */
+extern const char *newcamd_get_client_name(char *client_id);
 
 /* ===========================
  *       module-cccshare

trunk/module-newcamd.c
@@ -687,7 +687,6 @@ static int8_t newcamd_auth_client(IN_ADDR_T ip, uint8_t *deskey)
 {
     int32_t i, ok, rc, sid_list;
     uchar *usr = NULL, *pwd = NULL;
-    char *client_name = NULL;
     struct s_auth *account;
     uchar buf[14];
     uchar key[16];
@@ -734,7 +733,7 @@ static int8_t newcamd_auth_client(IN_ADDR_T ip, uint8_t *deskey)
     }
 
     snprintf(cl->ncd_client_id, sizeof(cl->ncd_client_id), "%02X%02X", mbuf[0], mbuf[1]);
-    client_name = get_ncd_client_name(cl->ncd_client_id);
+    const char *client_name = newcamd_get_client_name(cl->ncd_client_id);
 
     if(cl->ncd_proto==NCD_525 && 0x6D == mbuf[0]
        && 0x67 == mbuf[1] && 0x11 == cl->ncd_header[11])
@@ -1351,6 +1350,60 @@ static int32_t newcamd_recv_chk(struct s_client *client, uchar *dcw, int32_t *rc
       return -1;
   }
   return(idx);
+}
+
+/*
+ * resolve client type for newcamd protocol
+ */
+const char *newcamd_get_client_name(char *client_id)
+{
+	// When adding new entries keep the list sorted!
+	static const char const *ncd_service_ids[][2] = {
+		{ "0000", "generic" },
+		{ "0665", "rq-sssp-client/CS" },
+		{ "0666", "rqcamd" },
+		{ "0667", "rq-echo-client" },
+		{ "0669", "rq-sssp-client/CW" },
+		{ "0769", "JlsRq" },
+		{ "414C", "AlexCS" },
+		{ "4333", "camd3" },
+		{ "4343", "CCcam" },
+		{ "434C", "Cardlink" },
+		{ "4453", "DiabloCam/UW" },
+		{ "4543", "eyetvCamd" },
+		{ "4765", "Octagon" },
+		{ "4C43", "LCE" },
+		{ "4E58", "NextYE2k" },
+		{ "5342", "SBCL" },
+		{ "5456", "Tecview" },
+		{ "5644", "vdr-sc" },
+		{ "6378", "cx" },
+		{ "6502", "Tvheadend" },
+		{ "6576", "evocamd" },
+		{ "6762", "gbox2CS" },
+		{ "6B61", "Kaffeine" },
+		{ "6B63", "kpcs" },
+		{ "6D63", "mpcs" },
+		{ "6D67", "mgcamd" },
+		{ "6E65", "NextYE2k" },
+		{ "6E73", "NewCS" },
+		{ "7264", "radegast" },
+		{ "7363", "Scam" },
+		{ "7763", "WinCSC" },
+		{ "8888", "OSCam" },
+		{ "9911", "ACamd" },
+		{ NULL, NULL } };
+
+	int i = 0;
+	while (1) {
+		const char *id = ncd_service_ids[i][0];
+		if (!id)
+			break;
+		if (streq(id, client_id))
+			return ncd_service_ids[i][1];
+		i++;
+	}
+	return "unknown - please report";
 }
 
 void module_newcamd(struct s_module *ph)

trunk/oscam-simples.c
@@ -1185,34 +1185,6 @@ char *monitor_get_proto(struct s_client *cl)
 	return(ctyp);
 }
 
-/*
- * resolve clienttype for newcamdprotocol
- */
-char *get_ncd_client_name(char *client_id)
-{
-        static const int32_t max_id_idx = 32;
-        static const char const *ncd_service_ids[] = { "0000", "5644", "4C43", "4333", "7264", "6762", "6D67", "7763", "6E73", "6378", "6B61",
-                                           "6576", "4343", "5456", "414C", "0666", "0667", "9911", "434C", "4765", "5342",
-                                           "6E65", "4E58", "4453", "8888", "7363", "0669", "0665", "0769", "4543", "6D63",
-                                           "6B63", "6502" };
-
-        static char *ncd_service_names[] = { "generic", "vdr-sc", "LCE", "camd3", "radegast", "gbox2CS", "mgcamd", //actually a const so threadsafe
-                                             "WinCSC", "NewCS", "cx", "Kaffeine", "evocamd", "CCcam", "Tecview",
-                                             "AlexCS", "rqcamd", "rq-echo-client", "ACamd", "Cardlink", "Octagon", "SBCL",
-                                             "NextYE2k", "NextYE2k", "DiabloCam/UW", "OSCam", "Scam", "rq-sssp-client/CW",
-                                             "rq-sssp-client/CS", "JlsRq", "eyetvCamd", "mpcs", "kpcs", "Tvheadend", "unknown - please report" };
-
-        int32_t idx = 0;
-        for (idx = 0; idx <= max_id_idx; idx++) {
-		if(!memcmp(ncd_service_ids[idx], client_id, 4))
-                        return ncd_service_names[idx];
-
-        }
-
-        return ncd_service_names[max_id_idx+1];
-}
-
-
 void hexserial_to_newcamd(uchar *source, uchar *dest, uint16_t caid)
 {
   if (caid == 0x5581 || caid == 0x4aee) // Bulcrypt

commit 08718fe810ffd13c3de5369e58fddfbdbf661b69

Fix regression introduced in r7495 (cccam version was not shown).
This fixes ticket #2783.

git-svn-id: https://svn.streamboard.tv/oscam@7500 4b0bc96b-bc66-0410-9d44-ebda105a78c1

trunk/module-webif.c
@@ -2100,7 +2100,7 @@ static void webif_add_client_proto(struct templatevars *vars, struct s_client *c
 	}
 #endif
 #ifdef MODULE_CCCAM
-	if (streq(proto,"cccam")) {
+	if (strncmp(proto, "cccam", 5) == 0) {
 		struct cc_data *cc = cl->cc;
 		if (cc && cc->remote_version && cc->remote_build) {
 			tpl_printf(vars, TPLADDONCE, "CLIENTPROTO", "%s (%s-%s)", proto, cc->remote_version, cc->remote_build);